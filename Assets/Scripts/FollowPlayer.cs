9e987891377e10451a00c39b8f4450e610292dbe
Alain2131 <alain2131@live.ca>
2024-12-25 19:46:55 -0500

    Removed player reference from FollowPlayer.cs
    Instead, it fetches the player transform from GameManager.Instance.player.position

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,13 +2,11 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public Transform playerTransform;
-
     private Vector3 pos;
 
     void Update()
     {
-        pos = playerTransform.position;
+        pos = GameManager.Instance.player.position;
         pos.z = transform.position.z;
 
         transform.position = pos;
